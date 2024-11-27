7b83fd19bd9b091b2cec07c3a0c80616fbdff9dd
Adarsh Sharma <sharmaadarsh859@gmail.com>
2024-11-28 00:48:29 +0530

    Update ALLOWED_HOSTS for deployment

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -24,12 +24,7 @@ SECRET_KEY = "django-insecure-g3%=e5%v3s$uu$m^lp^c@^!e^x_q&fn^qa@3ta&ahn$w1#x-ee
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = [
-    "weather_application.onrender.com",
-    "https://weather-project-3dlm.onrender.com",
-    "localhost",
-]
+ALLOWED_HOSTS = ["weather-project-3dlm.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
